d6b051d8e59d2569386b2fc529643617560d7cfc
phegde <phegde@ipsoft.com>
2018-07-16 20:51:02 +0530

    added spacy dependancy

--- Util/find_ne.py
+++ Util/find_ne.py
@@ -6,10 +6,11 @@ def get_ne_chunks(text):
     locations = []
     for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(text))):
         if type(chunk) == Tree:
+            print(chunk)
             if chunk.label() == 'GPE':
                 ne = " ".join([token for token, pos in chunk.leaves()])
                 print('hello', ne)
                 locations.append(ne)
     return locations
 
-get_ne_chunks("Highest runs for New Zeland")
+get_ne_chunks("Number of runs for new zealand")
