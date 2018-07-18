197b3086d9f26108d9cdb9cf5e1487679a5139f4
phegde <phegde@ipsoft.com>
2018-07-18 18:18:49 +0530

    added training data for type classification(batting/bowling/fielding

--- ML/text_classifier.py
+++ ML/text_classifier.py
@@ -2,14 +2,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn import svm
 
-data = pd.read_csv("../Data/format_train.csv")
-data_txt = data['format']
+data = pd.read_csv("../Data/type_train.csv")
+data_txt = data['text']
 data_label = data['label']
 vectorizer = TfidfVectorizer(ngram_range=(1,2))
 vec = vectorizer.fit(data_txt)
 X_train = vec.transform(data_txt)
 Y_train = data.label
-X_test = vec.transform(["test", "one day", "t20"])
+X_test = vec.transform(["most runs in test", "highest wickets in an one day match", "most t20 sixes", "highest catch"])
 
 clf = svm.SVC(kernel='linear')
 clf.fit(X_train, Y_train)
