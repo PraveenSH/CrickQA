cc82bb1c545a470b6e49ea97b24694d14370b9df
phegde <phegde@ipsoft.com>
2018-07-18 18:43:39 +0530

    saving and loading ML models

--- ML/text_classifier.py
+++ ML/text_classifier.py
@@ -1,8 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from sklearn import svm
+import pickle
 
-data = pd.read_csv("../Data/type_train.csv")
+data = pd.read_csv("../Data/format_train.csv")
 data_txt = data['text']
 data_label = data['label']
 vectorizer = TfidfVectorizer(ngram_range=(1,2))
@@ -13,6 +14,7 @@ X_test = vec.transform(["most runs in test", "highest wickets in an one day matc
 
 clf = svm.SVC(kernel='linear')
 clf.fit(X_train, Y_train)
-
-preds = clf.predict(X_test)
+pickle.dump(clf,open("Models/format_model.sav",'wb'))
+loaded = pickle.load(open("Models/format_model.sav",'rb'))
+preds = loaded.predict(X_test)
 print(preds)
